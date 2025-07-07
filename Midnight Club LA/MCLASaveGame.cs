0f9f0c4bb2ea1d4a8efc1df94d8043da9929b2bb
Thierry <thierry44@gmail.com>
2025-07-07 00:07:42 -0400

    add midnight club la

---
+++ Midnight Club LA/MCLASaveGame.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace Rockstar
+{
+    internal struct MCLASaveEntry
+    {
+        internal string EntryStringID;
+        internal byte BuildVersion;
+        internal uint DataSize;
+        internal long Position;
+    }
+
+    internal class MCLACareer
+    {
+        private readonly EndianIO _io;
+        internal int Money;
+
+        internal MCLACareer(EndianIO io)
+        {
+            _io = io;
+            byte[] unknown = io.In.ReadBytes(4);
+            if(io.In.ReadByte() != 0x0B)
+                throw new Exception("Career version has changed. Ignoring load and starting a rage_new career");
+
+            uint ct = io.In.ReadUInt32();
+            io.Position += (ct*0x30);
+            Money = io.In.ReadInt32();
+        }
+    }
+
+    internal class MCLASaveGame
+    {
+        private readonly EndianIO _io;
+
+        internal List<MCLASaveEntry> Entries; 
+        internal int GameVersion;
+        internal int EntryCount;
+        internal MCLACareer Career;
+
+        internal MCLASaveGame(EndianIO io)
+        {
+            _io = io;
+            Read();
+        }
+
+        internal void Save()
+        {
+            
+        }
+
+        internal void Read()
+        {
+            GameVersion = _io.In.ReadInt32();
+            EntryCount = _io.In.ReadInt32();
+            Entries = new List<MCLASaveEntry>();
+
+            for (int i = 0; i < EntryCount; i++)
+            {
+                MCLASaveEntry saveEntry;
+                saveEntry.EntryStringID = _io.In.ReadAsciiString(_io.In.ReadInt32()); // we need 'Career'
+                saveEntry.BuildVersion = _io.In.ReadByte();
+                saveEntry.DataSize = _io.In.ReadUInt32();
+                saveEntry.Position = _io.Position;
+                _io.Position += saveEntry.DataSize;
+                Entries.Add(saveEntry);
+            }
+            _io.Position = Entries.Find(t => t.EntryStringID == "Career").Position;
+            Career = new MCLACareer(_io);
+        }
+    }
+
+}
---
+++ Midnight Club LA/MidnightClubLA.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Rockstar;
+
+namespace Horizon.PackageEditors.Midnight_Club_LA
+{
+    public partial class MidnightClubLA : EditorControl
+    {
+        private MCLASaveGame _saveGame;
+                
+        public MidnightClubLA()
+        {
+            InitializeComponent();
+            TitleID = FormID.MidnightClubLA;
+        }
+
+        public override bool Entry()
+        {
+            if (!OpenStfsFile("mc4.sav"))
+                return false;
+
+            _saveGame = new MCLASaveGame(IO);
+            DisplayData();
+
+            return true;
+        }
+
+        public override void Save()
+        {
+            _saveGame.Save();
+        }
+
+        private void DisplayData()
+        {
+            
+        }
+    }
+}
